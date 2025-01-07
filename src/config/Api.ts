4a0096764bbaacbe710300e54d4ad038e24b5ee9
Pankaj Kumar <c.thor269@gmail.com>
2025-01-07 11:02:57 +0530

    add

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const API_URL = "https://prime-game-store-backend.onrender.com";
+export const URL = "https://prime-game-store-backend.onrender.com";
 // change api
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  baseURL: URL, 
   headers: {
     'Content-Type': 'application/json',
   },

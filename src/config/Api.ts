664988468a90e0e9ff921a393538aa08369d7e80
Pankaj Kumar <c.thor269@gmail.com>
2025-01-31 12:08:23 +0530

    new changes

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const API_URL = "https://backend-prime-games.onrender.com";
-export const API_URL = "http://localhost:4444";
+export const API_URL = "https://backend-prime-games.onrender.com";
+// export const API_URL = "http://localhost:4444";
 export const api = axios.create({
   baseURL: API_URL, 
   headers: {

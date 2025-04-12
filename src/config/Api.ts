a318d57e5044fe56dcdf9b2cc60ac1c059e84566
Pankaj Kumar <c.thor269@gmail.com>
2025-04-12 15:01:11 +0530

    url

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:4444";
+export const API_URL = "https://backend-prime-games.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL, 

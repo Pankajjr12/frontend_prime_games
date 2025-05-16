6464579fdfb8585928eeb4e1214b45e6edde7c80
Pankaj Kumar <c.thor269@gmail.com>
2025-05-16 22:47:36 +0530

    chg

--- src/customer/pages/home/deal/DealCard.tsx
+++ src/customer/pages/home/deal/DealCard.tsx
@@ -6,9 +6,9 @@ const DealCard = ({deal}:{deal:Deal}) => {
   const navigate=useNavigate();
   return (
     <div onClick={()=>navigate(`/products/${deal.homeCategory?.categoryId}`)} className='w-full cursor-pointer'>
-    <img className='border-x-[7px] border-t-[7px] border-primary-color w-full h-[12rem] object-cover object-top' src={deal.homeCategory?.image} alt="" />
+    <img className='border-x-[7px] border-t-[7px] border-primary-color w-full h-[14rem] object-cover object-top' src={deal.homeCategory?.image} alt="" />
     <div className='border-4 border-black bg-black text-white p-1 text-center'>
-        <p className='text-lg font-semibold'>{deal.homeCategory?.categoryId.split("_").join(" ")}</p>
+       
         <p className='text-2xl font-bold'>{deal.discount}% OFF</p>
         <p className='text-balance text-lg'>shop now</p>
 
